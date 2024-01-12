c9cf8d2681ba38d34b204b04b5e053f5a7fba279
Natejpg <nathan11.abreu@gmail.com>
2024-01-12 01:08:48 -0300

    attempt #02 cors issue

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require("express")
 const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors');
+
 
 
 const quotesData = require('./data.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
 
@@ -15,13 +19,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running at port: ${port}`);
 })
-
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://quotesapi-c04u.onrender.com/"); // Update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-
 
