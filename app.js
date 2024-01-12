74555fdfa8f3c319c3276430e504a78b4e29c118
Natejpg <nathan11.abreu@gmail.com>
2024-01-12 00:21:25 -0300

    Fixing Attempt #3

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express")
+const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000;
 
 
+const quotesData = require('./data.json');
+
 app.get('/data.json', (req, res) => {
 
-    res.send({quotes});
+    res.send({quotesData});
 
 })
 
