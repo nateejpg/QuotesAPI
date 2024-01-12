c2ebe3e69c1b93610f888fe0bae180fe6d4f701a
Natejpg <nathan11.abreu@gmail.com>
2024-01-12 00:24:34 -0300

    fixing attempt #4

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-const quotesData = require('./data.json');
+let quotesData;
 
-app.get('/data.json', (req, res) => {
-
-    res.send({quotesData});
-
-})
+// Read the contents of data.json
+try {
+  const rawData = fs.readFileSync('./data.json');
+  quotesData = JSON.parse(rawData);
+} catch (error) {
+  console.error('Error reading or parsing data.json:', error.message);
+  process.exit(1); // Exit the application on error
+}
 
 app.listen(port, () => {
     console.log(`Server is running at port: ${port}`);
