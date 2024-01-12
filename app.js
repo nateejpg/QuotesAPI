bdc35ece9e50ea6a5a942e919b88ba19060c442e
Natejpg <nathan11.abreu@gmail.com>
2024-01-12 00:00:44 -0300

    trying smt

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import express from 'express';
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -397,5 +399,6 @@ app.get('/api/quotes', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running at port: ${port}`);
 })
+
 
 
